1d507ab923648d919d0434bbaa26042e85460bb5
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-21 17:33:09 -0300

    ap07(poo-p2): cacador aleatorio

--- Jogo.java
+++ Jogo.java
@@ -1,11 +1,27 @@
+import java.util.Random;
 public class Jogo {
   public static void main(String[] args) throws Exception {
+    var gerador = new Random();
     var personagem = new Personagem();
     personagem.nome = "John";
+    var soneca = new Personagem();
+    //nasce com muito e sem energia. fome mais ou menos
+    //ele tem probabilidade 80% de dormir
+    //10% de caçar
+    //10% de comer
     while(true){
-      personagem.cacar();
-      personagem.comer();
-      personagem.dormir();
+      int oQueFazer = gerador.nextInt(3); //[0..3[
+      switch(oQueFazer){
+        case 0:
+          personagem.cacar();
+          break;
+        case 1:
+        personagem.comer();
+          break;
+        case 2:
+        personagem.dormir();
+        break;
+      }
       System.out.printf(
         "e: %d, f: %d, s: %d\n",
         personagem.energia,
