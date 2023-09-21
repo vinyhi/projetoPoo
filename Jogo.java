cf4c76976fb2e783d0a4c00256e251fe04f5e811
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-21 18:27:05 -0300

    ap07(poo-p2): encapsulamento e baixo acoplamento

--- Jogo.java
+++ Jogo.java
@@ -2,17 +2,12 @@ import java.util.Random;
 public class Jogo {
   public static void main(String[] args) throws Exception {
     var gerador = new Random();
-    var personagem = new Personagem();
-    personagem.nome = "John";
-    var soneca = new Personagem();
+    var personagem = new Personagem("John");
+    var soneca = new Personagem("Soneca", 0, 4, 10);
     //nasce com muito sono e sem energia. fome mais ou menos
     //ele tem probabilidade 80% de dormir
     //10% de caçar
     //10% de comer
-    soneca.nome = "Soneca";
-    soneca.sono = 10;
-    soneca.energia = 0;
-    soneca.fome = 4;
     while(true){
       int oQueFazer = gerador.nextInt(3); //[0..3[
       switch(oQueFazer){
@@ -26,12 +21,7 @@ public class Jogo {
         personagem.dormir();
         break;
       }
-      System.out.printf(
-        "e: %d, f: %d, s: %d\n",
-        personagem.energia,
-        personagem.fome,
-        personagem.sono
-      );
+      personagem.exibirEstado();
       System.out.println("--------------------");
       var oQueOSonecaVaiFazer = gerador.nextDouble();
       if (oQueOSonecaVaiFazer <= 0.8)
@@ -40,12 +30,7 @@ public class Jogo {
         soneca.comer();
       else
         soneca.cacar();
-      System.out.printf(
-        "e: %d, f: %d, s: %d\n",
-        soneca.energia,
-        soneca.fome,
-        soneca.sono
-      );
+      soneca.exibirEstado();
       System.out.println("********************");
       Thread.sleep(8000);
     }
--- Personagem.java
+++ Personagem.java
@@ -1,10 +1,30 @@
 public class Personagem {
  
   //atributos ou propriedades
-  String nome;
-  int energia = 10;
-  int fome = 0;
-  int sono = 0;
+  private String nome;
+  private int energia = 10;
+  private int fome = 0;
+  private int sono = 0;
+
+  //construtor: lista de parâmetros vazia
+  // Personagem(){
+
+  // }
+
+  Personagem(String nome){
+    this.nome = nome;
+  }
+
+  Personagem(String nome, int energia, int fome, int sono){
+    this.nome = nome;
+    if (energia >= 0 && energia <= 10)
+      this.energia = energia;
+    if (fome >= 0 && fome <= 10)
+      this.fome = fome;
+    if(sono >= 0 && sono <= 10)
+      this.sono = sono;  
+  }
+
   //comportamentos (métodos)
   void cacar(){
     if(energia >= 2){
@@ -42,7 +62,14 @@ public class Personagem {
 
   }
 
-  public static void main(String[] args) {
-    
+  void exibirEstado(){
+    System.out.printf(
+      "%s: e: %d, f: %d, s: %d\n",
+      nome,
+      energia,
+      fome,
+      sono
+    );
   }
+ 
 }
